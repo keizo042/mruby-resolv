46ba777c60bdc0198b0a1ca3e88c161c895489f5
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-09-15 20:11:59 +0900

    Add example

---
+++ example/simple.rb
@@ -0,0 +1,23 @@
+# prepare data
+hdr = Resolv::DNS::Query::Header.new(
+    0x42,
+    Resolv::DNS::Query::Header::QR::CLIENT,
+    Resolv::DNS::Query::Header::OPCODE::QUERY,
+    0,
+    0,
+    Resolv::DNS::Query::Header::RD::DESIRE,
+    0,
+    Resolv::DNS::Query::Header::RCODE::NoError,
+    1, 0, 0, 0)
+
+q = Resolv::DNS::Query.new(
+  hdr,
+  [Resolv::DNS::Query::Question.new("google.com", 1,1)],
+    [],
+    [],
+    [])
+
+dns = Resolv::DNS.new
+dns.send(q)
+ret = dns.recv
+# ret is a `Reolv::DNS::Query` as DNS Response, as you want
