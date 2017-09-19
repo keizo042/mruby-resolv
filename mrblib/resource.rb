fa56ef3c08ab32f2b7d52308d6ca70ea41668029
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-09-19 23:35:05 +0900

    WIP

--- mrblib/resource.rb
+++ mrblib/resource.rb
@@ -2,10 +2,6 @@ class Resolv
   class DNS
     class Resource
       TypeValue = 0
-      #
-      # DNS RR abstract "type" classes
-      # 
-
       class MX < Resolv::DNS::Resource
         TypeValue = 15
       end
@@ -18,6 +14,10 @@ class Resolv
         TypeValue = 12
       end
 
+      class OPT < Resolv::DNS::Resource
+        TypeValue = 41
+      end
+
       class ANY < Resolv::DNS::Resource
         TypeValue = 255
       end
@@ -25,12 +25,27 @@ class Resolv
       # CLASS INTERNET module
       module IN
         ClassValue = 1
+        #
+        # Common Resource Record Definition
+        #
+        [NS, MX, PTR, ANY].each do
+        end
         class A < Resolv::DNS::Resource
           TypeValue = 1
           ClassValue = IN::ClassValue
         end
         class AAAA < Resolv::DNS::Resource
           TypeValue = 28
+          ClassValue = IN::ClassValue
+        end
+
+        class  SRV < Resolv::DNS::Resource
+          TypeValue = 33
+          ClassValue = IN::ClassValue
+        end
+
+        class WKS < Resolv::DNS::Resource
+          TypeValue = 11
           ClassValue = IN::ClassValue
         end
       end
